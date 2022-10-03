b70676d039657ce261683b95d94a2a8372c92616
albim72 <marcin.albiniak72@gmail.com>
2022-10-03 12:02:35 +0200

    Create input.py

---
+++ DZIEN_1/input.py
@@ -0,0 +1,8 @@
+print("podaj swoje imię: ")
+imie = input()
+print("miło Cię widzieć",imie)
+print("liczba znaków w imieniu:",len(imie))
+
+wiek = int(input("podaj swój wiek w latach: "))
+print("wiek [lata] = ",wiek)
+print("wiek za 10 lat:",(wiek+10))
