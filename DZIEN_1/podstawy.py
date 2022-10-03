5595d3b5c5bcc661f962f615d0379801e4ad84da
albim72 <marcin.albiniak72@gmail.com>
2022-10-03 11:20:42 +0200

    Create podstawy.py

---
+++ DZIEN_1/podstawy.py
@@ -0,0 +1,19 @@
+print("to jest pierwsza linia programu!")
+
+a = 45
+
+print(a)
+print(type(a))
+
+a = "wartość tekstowa"
+print(a)
+print(type(a))
+
+b:float
+b=8.458
+print(b)
+print(type(b))
+
+b = True
+print(b)
+print(type(b))
