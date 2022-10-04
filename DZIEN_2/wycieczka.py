69e7beed2f46b19889aec68df72dcfa157d2bf5a
albim72 <marcin.albiniak72@gmail.com>
2022-10-04 10:23:33 +0200

    Update wycieczka.py

--- DZIEN_2/wycieczka.py
+++ DZIEN_2/wycieczka.py
@@ -4,3 +4,14 @@
 #pozwalaj tylko na wprowadzenie rabatu z zakresu (1-70%)
 #blokuj roziwązanie dla innych wartości
 #wartość 0 dla rabatu uczyńmy wartością domyślną parametru rabat (rabat=0)
+
+
+def kwota(t,nw,w,u,i,rab=0):
+    if rab>=0 and rab <= 70:
+        return (nw+t)*(1-rab/100)+w+u+i
+    else:
+        return "podaj rabat mniejszy niż 71% lub większy od 0"
+
+print(f"{kwota(100,100,50,50,50):.2f} zł")
+print(f"{kwota(100,100,50,50,50,25):.2f} zł")
+print(f"{kwota(100,100,50,50,50,75)} zł")
