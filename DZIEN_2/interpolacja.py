3973f667d195b31065b8a1eb3c9b6a496e8a7575
albim72 <marcin.albiniak72@gmail.com>
2022-10-04 13:22:02 +0200

    Update interpolacja.py

--- DZIEN_2/interpolacja.py
+++ DZIEN_2/interpolacja.py
@@ -21,3 +21,24 @@ w = 86.467743
 
 formatowanie = '%-30s = %.2f' %(nz,w)
 print(formatowanie)
+
+owoce = [
+      ('awokado',7.88),
+      ('banan',4.99),
+      ('jabłko',3.41),
+      ('mandarynka',9.99),
+      ('malina',12.11)
+]
+
+#stwórz cennik wg wzoru (linia cennika) -> #nr: nazwa(10) = 0.00 zł
+print("__________________________________________")
+for i,(nazwa,cena) in enumerate(owoce):
+      print('#%d: %-10s = %6.2f zł' %(i,nazwa,cena))
+
+print("__________________________________________")
+for i,(nazwa,cena) in enumerate(owoce):
+      print('#%d: %-10s = %6.2f zł' %(
+            i+1,
+            nazwa.title(),
+            round(cena,1)
+      ))
