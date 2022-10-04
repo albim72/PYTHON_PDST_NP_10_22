1d997e25468ebb56b1ec3cf01b43a123aca244b2
albim72 <marcin.albiniak72@gmail.com>
2022-10-04 09:09:47 +0200

    Update moje_funkcje.py

--- DZIEN_1/moje_funkcje.py
+++ DZIEN_1/moje_funkcje.py
@@ -6,3 +6,12 @@ def powitanie():
 
 powitanie()
 powitanie()
+
+#komentowanie/odkomentowanie wielu linii: zaznacz blok, wciśnij kobinajcję: CTRL + /
+
+# for _ in range(1,26):
+#     powitanie()
+
+for i in range(1,26,3):
+    print("____",i,"____")
+    powitanie()
