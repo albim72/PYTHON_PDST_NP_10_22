785d1fee0ce6613a9c5c02dc19ec10874d470c1b
albim72 <marcin.albiniak72@gmail.com>
2022-10-04 10:11:23 +0200

    Update wycieczka.py

--- DZIEN_2/wycieczka.py
+++ DZIEN_2/wycieczka.py
@@ -1,1 +1,6 @@
+#funkcja kwota(transport,nocleg_wyz,wycieczki,ubezp,inne,rabat) ->
+#(transport + nocleg_wyz)*(1-rabat/100)+wycieczki+ubezp+inne
 
+#pozwalaj tylko na wprowadzenie rabatu z zakresu (1-70%)
+#blokuj roziwązanie dla innych wartości
+#wartość 0 dla rabatu uczyńmy wartością domyślną parametru rabat (rabat=0)
