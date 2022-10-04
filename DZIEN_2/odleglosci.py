07a0699abe28f83104b42571b4b86295c07d011a
albim72 <marcin.albiniak72@gmail.com>
2022-10-04 11:10:42 +0200

    Create odleglosci.py

---
+++ DZIEN_2/odleglosci.py
@@ -0,0 +1,6 @@
+#napisz funckję przeliczającą metry na miary anglosaskie
+#użyj funckji input() do pobraania danych: odległść [m], rodzaj miary (1-5)
+#pobrane wartość podstaw do fukcji i przetestuj ją
+#opcjonalnie -> użyj słownika do przekazania miar oraz ich przeliczników i nazw
+#przelicznik = {1:[przelicznik, nazwa_miary], 2:[przelicznik, nazwa_miary],}
+#wyliczenie miary -> Pdleglosc_metry/wspolczynnik
