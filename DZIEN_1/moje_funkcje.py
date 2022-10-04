d8b33537551a80ae224bba76318c5b2c87fb1ea3
albim72 <marcin.albiniak72@gmail.com>
2022-10-04 09:36:53 +0200

    Update moje_funkcje.py

--- DZIEN_1/moje_funkcje.py
+++ DZIEN_1/moje_funkcje.py
@@ -15,3 +15,30 @@ powitanie()
 for i in range(1,26,3):
     print("____",i,"____")
     powitanie()
+
+#druga fuknkcja - funkcja z parametrami
+def obywatel(nrtelefonu,kraj="Polska"):
+    print("kraj pochodzenia:",kraj,", numer telefonu:",nrtelefonu)
+
+obywatel(5345345435,"Rumunia")
+obywatel(5654656546,"Laos")
+obywatel(5656535565,"Boliwia")
+obywatel(7876767677,"Norwegia")
+obywatel(4363456566)
+
+#trzecia funkcja - zasięg zmiennych
+
+def gx(n):
+    return n**5
+
+f = 100
+def policz(a,b,x):
+    global f
+    f = (a+b)*x + f + gx(b)
+    return f
+#f =250 - zmienna niedostępna dla programu ze względu na działanie zmiennej globalnej f zdefiniowanej w funkcji
+print(policz(3,6,2))
+
+wynik = policz(4.5,6.7,8)
+print(wynik)
+print(f)
