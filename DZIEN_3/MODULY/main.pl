8699a221d83847f51254b16a5d4edaaf00ddc18b
albim72 <marcin.albiniak72@gmail.com>
2022-10-05 15:34:00 +0200

    Update main.pl

--- DZIEN_3/MODULY/main.pl
+++ DZIEN_3/MODULY/main.pl
@@ -4,9 +4,19 @@
 from dane import book
 from funkcje import biblioteka, hx
 
+#from miasto import Miasto
+from stolica import Stolica, Miasto
+
 print(book)
 print(book["tytul"])
 
 print(hx(6,9))
 
 print(biblioteka("Gniezno",34,book['tytul']))
+
+m = Miasto(56,"Zamość","lubelskie")
+m.print_miasto()
+
+s = Stolica(1,"Warszawa","mazowieckie","Jan Kot")
+s.print_miasto()
+s.prezydentinfo()
