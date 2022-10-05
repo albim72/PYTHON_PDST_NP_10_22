3d1708fd0cdb41680b2a6172cce3cdeefe037090
albim72 <marcin.albiniak72@gmail.com>
2022-10-05 11:30:03 +0200

    Update def_error.py

--- DZIEN_3/def_error.py
+++ DZIEN_3/def_error.py
@@ -4,3 +4,7 @@ def policz(x,y,z):
 print(policz(3,6,7))
 print(policz(2.2,8.9,7))
 print(policz(0.00056,222,12E+56))
+
+#przekształć program w ten sposób żeby aktualnej sytuacji nie przzerywał programu z kodem 1
+#tylko wyświetlał komunikat: wprowadż trzy parametry do funkcji policz(x,y,z)
+#ostatecznie wyjście z programu z kodem 0
