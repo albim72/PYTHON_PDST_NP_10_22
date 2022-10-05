51c1d0a3aec25d165f7c6af22ce5c7b59d410ece
albim72 <marcin.albiniak72@gmail.com>
2022-10-05 11:36:58 +0200

    Update def_error.py

--- DZIEN_3/def_error.py
+++ DZIEN_3/def_error.py
@@ -1,10 +1,19 @@
 def policz(x,y,z):
     return x**2+y**2+z**2
+try:
+    print(policz(3,6,7))
+    print(policz(2.2,8.9,7))
+    print(policz(0.00056,222,12E+56))
 
-print(policz(3,6,7))
-print(policz(2.2,8.9,7))
-print(policz(0.00056,222,12E+56))
+    print(policz(1,1,True))
+    print(policz(1,1))
+
+except TypeError:
+    print("wprowadż trzy parametry do funkcji policz(x,y,z)")
+
+print(policz(5,6,8))
 
 #przekształć program w ten sposób żeby aktualnej sytuacji nie przzerywał programu z kodem 1
 #tylko wyświetlał komunikat: wprowadż trzy parametry do funkcji policz(x,y,z)
 #ostatecznie wyjście z programu z kodem 0
+
