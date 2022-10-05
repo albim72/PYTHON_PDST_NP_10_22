2d2e6cc98987146f99c5aaa13c39cc37fc208bb6
albim72 <marcin.albiniak72@gmail.com>
2022-10-05 14:13:53 +0200

    Update spalanie.py

--- DZIEN_3/spalanie.py
+++ DZIEN_3/spalanie.py
@@ -9,3 +9,13 @@ class IPojzad:
 
     @abstractmethod
     def kosztyprzejzadu(self,litry,odl,cena_l):raise NotImplementedError
+
+        
+        
+class Pojazd(IPojzad):
+
+    def spalanie(self, litry, odl):
+        return litry*100/odl
+
+    def kosztyprzejzadu(self, litry, odl, cena_l):
+        return self.spalanie(litry,odl)*(odl/100)*cena_l
