18cb2b1ce94a602ed5521ba55332bd8334d31a6b
albim72 <marcin.albiniak72@gmail.com>
2022-10-05 09:37:12 +0200

    Update osoby.py

--- DZIEN_2/osoby.py
+++ DZIEN_2/osoby.py
@@ -82,3 +82,29 @@ class Sport:
 
 class Dodatkowa:
     pass
+
+class Student(Pracownik,Sport,Dodatkowa):
+    
+    #konstruktor z wielodziedziczeniem
+    def __init__(self,imie,wiek,wzrost,waga,nr_studenta,wydzial,kierunek,rok_stud,
+                 firma="",stanowisko="",latapracy="",wynagrodzenie="",dyscyplina="",lata_upr="",best_wynik=""):
+        Pracownik.__init__(self,imie,wiek,wzrost,waga,firma,stanowisko,latapracy,wynagrodzenie)
+        Sport.__init__(self,dyscyplina,lata_upr,best_wynik)
+        self.nr_studenta = nr_studenta
+        self.wydzial = wydzial
+        self.kierunek = kierunek
+        self.rok_stud = rok_stud
+        
+        
+    def print_student(self):
+        print(f"student nr {self.nr_studenta}, wydział: {self.wydzial}, kierunek: {self.kierunek}, "
+              f" rok studiów: {self.rok_stud}.")
+
+    def czypracownik(self):
+        return self.firma != ""
+
+    # def czypracownik(self):
+    #     if self.firma == "":
+    #         return False
+    #     else:
+    #         return True
