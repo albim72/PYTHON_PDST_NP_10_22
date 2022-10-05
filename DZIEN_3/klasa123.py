86dade8fd4a8fb9add7ffc397a72a23f3403f40e
albim72 <marcin.albiniak72@gmail.com>
2022-10-05 10:29:24 +0200

    Update klasa123.py

--- DZIEN_3/klasa123.py
+++ DZIEN_3/klasa123.py
@@ -1,20 +1,36 @@
 class PierwszaKlasa:
-    
+
     def __init__(self,a,b):
         self.a = a
         self.b = b
-        
+
     def print_ab(self):
         print(f"Klasa PierwszaKlasa -> a={self.a}, b={self.b}")
-        
+
 class DrugaKlasa(PierwszaKlasa):
-    
+
     def __init__(self,a,b,c):
         super().__init__(a,b)
         self.c=c
-        
+
     def print_abc(self):
         print(f"Klasa DrugaKlasa -> a={self.a}, b={self.b}, c={self.c}")
-        
+
     def suma_elementow(self):
         return self.a+self.b+self.c
+    
+    
+class TrzeciaKlasa(DrugaKlasa):
+    
+    def __init__(self,a,b,c,d):
+        super().__init__(a,b,c)
+        self.d=d
+    
+    def print_abcd(self):
+        print(f"Klasa TrzeciaKlasa -> a={self.a}, b={self.b}, c={self.c}, d={self.d}")
+
+    def suma_elementow(self):
+        return super().suma_elementow() + self.d
+        
+    
+    
