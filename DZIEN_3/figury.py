90dbbbd08478c7ca484fcd741d519c0dce89e9ee
albim72 <marcin.albiniak72@gmail.com>
2022-10-05 14:01:01 +0200

    Update figury.py

--- DZIEN_3/figury.py
+++ DZIEN_3/figury.py
@@ -32,3 +32,15 @@ print(f"pole trójkąta wynosi: {tr.policz_pole():.2f}")
 
 #Stwórz klasę Kolo dziedziczącą klasę Figura, policz pole koła dla promienia równego 5.5
 #pole koła -> math.pi*a**2
+
+class Kolo(Figura):
+
+    def __init__(self,a):
+        super().__init__(a,0)
+
+    def policz_pole(self):
+        return math.pi*self.a**2
+
+
+kl = Kolo(5.5)
+print(f"pole koła wynosi: {kl.policz_pole():.2f}")
