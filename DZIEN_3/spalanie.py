45efbc91a4527bb48c5ebc884b91d0e4f0fb39c0
albim72 <marcin.albiniak72@gmail.com>
2022-10-05 15:05:21 +0200

    Update spalanie.py

--- DZIEN_3/spalanie.py
+++ DZIEN_3/spalanie.py
@@ -25,13 +25,21 @@ class IDanePojazdu:
     def silnik(self):raise NotImplementedError
 
 
-#zaimplementuj klasę  IDanPojazdu jako drugą klasę w klasie Pojazd
+#zaimplementuj klasę  IDanePojazdu jako drugą klasę w klasie Pojazd
 #w klasie Pojzad - stwórz konstruktor(marka,model,rocznik,rodzaj_silnika,poj,moc)
 #zaimplementuj metodę -> opispojazdu() -> wypisz wartości konstruktora: marka,model,rocznik
 #zaimplementuj metodę -> silnik() -> wypisz wartości konstruktora: rodzaj_silnika,poj,moc
 
 #wyświelt dane pojazdu przed wyznaczeniem kosztów spalania
-class Pojazd(IPojzad):
+class Pojazd(IPojzad,IDanePojazdu):
+
+    def __init__(self,marka,model,rocznik,rodzaj_silnika,poj,moc):
+        self.marka = marka
+        self.model = model
+        self.rocznik = rocznik
+        self.rodzaj_silnika = rodzaj_silnika
+        self.poj = poj
+        self.moc = moc
 
     def spalanie(self, litry, odl):
         return litry*100/odl
@@ -39,13 +47,20 @@ class Pojazd(IPojzad):
     def kosztyprzejzadu(self, litry, odl, cena_l):
         return self.spalanie(litry,odl)*(odl/100)*cena_l
 
+    def opispojazdu(self):
+        print(f"Pojazd -> marka: {self.marka}, model: {self.model}, rocznik: {self.rocznik}")
 
-p1 = Pojazd()
+    def silnik(self):
+        print(f"Silnik -> rodzaj silnika: {self.rodzaj_silnika}, pojemność: {self.poj}, moc: {self.moc} kM")
+
+
+p1 = Pojazd("Toyota","Avensis",2019,"Benzyna",2.0,150)
 lt = float(input("podaj ile litrów paliwa spalił samochód podczas przejazdu: "))
 odl = float(input("podaj długość trasy: "))
 cn = float(input("podaj cenę za litr paliwa: "))
 
 #opis pojazdu
-
+p1.opispojazdu()
+p1.silnik()
 print(f"spalanie [l/100km] wynosi: {p1.spalanie(lt,odl):.2f}")
 print(f"koszty przejazdu na trasie: {odl} km wynoszą {p1.kosztyprzejzadu(lt,odl,cn):.2f} zł")
