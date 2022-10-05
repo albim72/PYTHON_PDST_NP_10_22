8ac91fb02c37e721f7105bf51bd6ea3784b5b3d2
albim72 <marcin.albiniak72@gmail.com>
2022-10-05 15:21:16 +0200

    Update main.pl

--- DZIEN_3/MODULY/main.pl
+++ DZIEN_3/MODULY/main.pl
@@ -1,1 +1,12 @@
+#import dane
+#import dane as dn
 
+from dane import book
+from funkcje import biblioteka, hx
+
+print(book)
+print(book["tytul"])
+
+print(hx(6,9))
+
+print(biblioteka("Gniezno",34,book['tytul']))
