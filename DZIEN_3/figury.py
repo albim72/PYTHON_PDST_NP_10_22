91d0c48b955bd954438089f91f16207a031c8cf4
albim72 <marcin.albiniak72@gmail.com>
2022-10-05 13:45:40 +0200

    Update figury.py

--- DZIEN_3/figury.py
+++ DZIEN_3/figury.py
@@ -23,3 +23,10 @@ class Trojkat(Figura):
         return self.a*self.b/2
 
 
+pr = Prostokat(5.6,7.8)
+print(f"pole prostokąta wynosi: {pr.policz_pole():.2f}")
+
+tr = Trojkat(6.8,5.9)
+print(f"pole trójkąta wynosi: {tr.policz_pole():.2f}")
+
+
