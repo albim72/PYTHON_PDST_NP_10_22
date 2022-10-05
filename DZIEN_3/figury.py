75a83217ada8056db68fa515dd99ac3d98f856ec
albim72 <marcin.albiniak72@gmail.com>
2022-10-05 13:42:21 +0200

    Update figury.py

--- DZIEN_3/figury.py
+++ DZIEN_3/figury.py
@@ -1,11 +1,25 @@
 from abc import ABC,abstractmethod
 
 class Figura(ABC):
-    
+
     def __init__(self,a,b):
         self.a=a
         self.b=b
-        
+
     @abstractmethod
     def policz_pole(self):
         pass
+
+
+class Prostokat(Figura):
+
+    def policz_pole(self):
+        return self.a*self.b
+
+
+class Trojkat(Figura):
+
+    def policz_pole(self):
+        return self.a*self.b/2
+
+
