c02e80a2b0fdc495dbb625408121142c856034dc
albim72 <marcin.albiniak72@gmail.com>
2022-10-05 10:23:33 +0200

    Update klasa123.py

--- DZIEN_3/klasa123.py
+++ DZIEN_3/klasa123.py
@@ -1,1 +1,8 @@
-
+class PierwszaKlasa:
+    
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
+        
+    def print_ab(self):
+        print(f"Klasa PierwszaKlasa -> a={self.a}, b={self.b}")
