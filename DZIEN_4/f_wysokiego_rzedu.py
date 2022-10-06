a8f2854ba419167bf65160c33592046c90e63427
albim72 <marcin.albiniak72@gmail.com>
2022-10-06 10:50:55 +0200

    Update f_wysokiego_rzedu.py

--- DZIEN_4/f_wysokiego_rzedu.py
+++ DZIEN_4/f_wysokiego_rzedu.py
@@ -22,3 +22,27 @@ print(filtrowanie(ddtest,czyToSuperLiczba))
 print(filtrowanie(sltest,czyToSuperLiczba)) #problem z odczytem value()
 
 #jak napisać funkcję, która obsłuży słownik i jego wartości a nie klucze...
+
+#funckja mapująca na podstawie działania opisanego inną funkcją....
+#Przykład 2
+
+#parametr transformacja jest funkcją!!!
+def mapowanie(dane,tranformacja):
+    nowe_wart = []
+    for element in dane:
+        nowe_wart.append(tranformacja(element))
+    return nowe_wart
+
+def transformata_obl(nb):
+    return (nb+2)**2
+
+#pomniejsz 3-krotnie każdą wartość zestawu danych
+def pomniejszanie(nb):
+    return nb/3
+print(mapowanie(danetestowe,transformata_obl))
+print(mapowanie(ddtest,transformata_obl))
+print(mapowanie(sltest,transformata_obl))
+
+print(mapowanie(danetestowe,pomniejszanie))
+print(mapowanie(ddtest,pomniejszanie))
+print(mapowanie(sltest,pomniejszanie))
