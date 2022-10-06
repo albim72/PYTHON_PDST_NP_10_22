f5739e16a8d344bab6ac6664012154b1341a39cc
albim72 <marcin.albiniak72@gmail.com>
2022-10-06 10:24:31 +0200

    Create zip.py

---
+++ DZIEN_4/zip.py
@@ -0,0 +1,11 @@
+liczby = [1,2,3,4,5]
+liczby_pl = ['jeden','dwa','trzy','cztery','pięć']
+liczby_eng = ['one','two','three','four','five']
+
+wynik = zip()
+wynik_lista = list(wynik)
+print(wynik_lista)
+
+wynik = zip(liczby,liczby_pl,liczby_eng)
+wynik_lista = list(wynik)
+print(wynik_lista)
