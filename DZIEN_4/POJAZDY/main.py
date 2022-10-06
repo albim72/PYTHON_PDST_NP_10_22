9bf6b023190ec767b4416be4ac91dd991d9f27d8
albim72 <marcin.albiniak72@gmail.com>
2022-10-06 09:56:49 +0200

    Update main.py

--- DZIEN_4/POJAZDY/main.py
+++ DZIEN_4/POJAZDY/main.py
@@ -1,1 +1,18 @@
+from rower import Rower
+from samochod import Samochod
 
+rw = Rower("Trek","MadOne",2020,"korba","czerwony","shimano",12.4,True)
+
+print(rw.daneroweru())
+print(rw.naped())
+
+rw.set_waga_ramy(13.7)
+print(rw.daneroweru())
+
+print("______________________________________________")
+
+sm = Samochod("Opel","Insignia",2019,"silnik diesel","czarny",2.2,212,True)
+print(f"prędkość masymalna: {sm.vmax()} km/h")
+print(sm.naped())
+sm.set_moc(219)
+print(sm.naped())
