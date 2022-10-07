b72d234dfbd447d3a631d5fa853af95cb8ea48c1
albim72 <marcin.albiniak72@gmail.com>
2022-10-07 09:51:00 +0200

    Update minidom.py

--- DZIEN_5/minidom.py
+++ DZIEN_5/minidom.py
@@ -3,6 +3,9 @@ from xml.dom.minidom import parse
 dom = parse("dane.xml")
 name = dom.getElementsByTagName('name')
 kod = dom.getElementsByTagName('kod')
+adres = dom.getElementsByTagName('url')
 
 print(" ".join(t.nodeValue for t in name[0].childNodes if t.nodeType == t.TEXT_NODE))
+print(" ".join(t.nodeValue for t in adres[0].childNodes if t.nodeType == t.TEXT_NODE))
 print(" ".join(t.nodeValue for t in kod[0].childNodes if t.nodeType == t.TEXT_NODE))
+print(" ".join(t.nodeValue for t in kod[1].childNodes if t.nodeType == t.TEXT_NODE))
