4d820dde541c30f48322b123544d9f0381b375e3
albim72 <marcin.albiniak72@gmail.com>
2022-10-07 10:34:04 +0200

    Update create_xml_file.py

--- DZIEN_5/create_xml_file.py
+++ DZIEN_5/create_xml_file.py
@@ -41,3 +41,30 @@ pod.text = '4'
 
 klima = SubElement(wyp_dod,"klima")
 klima.text = 'GTR4535'
+
+#drugi rekord -> sam2
+
+sam2 = SubElement(top,"samochod")
+
+id = SubElement(sam2,"id")
+id.text = 'sam2'
+
+marka = SubElement(sam2,"marka")
+marka.text = 'Subaru'
+
+model = SubElement(sam2,"model")
+model.text = 'Outback'
+
+poj = SubElement(sam2,"pojemnosc")
+poj.text = '2.4'
+
+cena = SubElement(sam2,"cena")
+id.text = '126970'
+
+wyp_dod = SubElement(sam2,"wyp_dodatkowe")
+
+kolor = SubElement(wyp_dod,"kolor")
+kolor.text = 'czerwony metallic'
+
+klima = SubElement(wyp_dod,"klima")
+klima.text = 'VBBN785'
