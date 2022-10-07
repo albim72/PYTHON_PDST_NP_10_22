be7bf83f1d02e25e4af36916fb928b893abc7120
albim72 <marcin.albiniak72@gmail.com>
2022-10-07 08:56:25 +0200

    Create contextmanager.py

---
+++ DZIEN_5/contextmanager.py
@@ -0,0 +1,17 @@
+#Schemat Context Managera
+
+class ContextManager:
+
+    def __init__(self):
+        print('inicjacja konstruktora init()')
+
+    def __enter__(self):
+        print('inicjacja konstruktora enter()')
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print('inicjacja konstruktora exit()')
+
+
+with ContextManager() as manager:
+    print('to jest właściwe działanie managera....')
