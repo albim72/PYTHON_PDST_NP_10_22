d66e016470100b923dadca6d46f367b9e22eedc4
albim72 <marcin.albiniak72@gmail.com>
2022-10-07 08:30:26 +0200

    Update jsonfile.py

--- DZIEN_5/jsonfile.py
+++ DZIEN_5/jsonfile.py
@@ -1,3 +1,5 @@
+import json
+
 json_str = '{"imiona":["Jan","Leon"],"nazwisko":"Kot","data_urodzenia":"1974-10-09","liczba_punktow":45}'
 
 print(json_str)
@@ -15,3 +17,25 @@ print(type(json_d))
 print(json_d["liczba_punktow"])
 
 print(f"pierwsze imię: {json_d['imiona'][0]}, nazwisko: {json_d['nazwisko']}")
+
+sam = {
+    "marka":"Opel",
+    "model":"Insignia",
+    "rocznik":2020,
+    "poj":2.2
+}
+
+jsonauto = json.dumps(sam,indent=4)
+
+print(jsonauto)
+print(type(jsonauto))
+
+with open("samochod.json","w",encoding="utf-8") as f:
+    f.write(jsonauto)
+
+with open("samochod.json","r",encoding="utf-8") as f:
+    auto_dict = json.load(f)
+
+print(auto_dict)
+print(auto_dict["marka"])
+
